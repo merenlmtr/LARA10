204d9d0406523eca1c0998f19c6e3855466b5fbb
merenlmtr <merenltr@gmail.com>
2023-11-08 00:49:06 +0530

    user avatar delete function

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AvatarUpdateRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(AvatarUpdateRequest $request){
 
         $path = $request->file('avatar')->store('avatars', 'public');
+        if($old_avatar = $request->user()->avatar){
+            Storage::disk('public')->delete($old_avatar);
+        }
         auth()->user()->update(['avatar' => $path]);
 
         return back()->with("success","Avatar updated successfully");
